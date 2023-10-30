262868eab02f8a03db4fe6d701826cb19eeb3c55
aufaramdhn <ramadhanaufa3@gmail.com>
2023-10-30 19:43:31 +0700

    update bg color

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
 import LinkCard from "./components/LinkCard";
 import profile from "/avatar.png";
 import "remixicon/fonts/remixicon.css";
+import { motion } from "framer-motion";
 
 function App() {
   return (
-    <section className="bg-black">
-      <div className="w-[500px] bg-white mx-auto min-h-[120vh]">
+    <section>
+      <div className="w-[500px] bg-slate-100 mx-auto min-h-[120vh]">
         <div className="p-20">
           <div className="relative bg-slate-600 h-44 rounded-2xl">
             <div className="relative top-0 left-0 w-[118px] h-[48px] bg-transparent rounded-br-2xl inverted-border">
               <div className="absolute flex items-center justify-center gap-2 px-3 py-1 bg-red-500 rounded-xl top-2">
-                <div className="w-3 h-3 bg-green-500 rounded-full"></div>
+                <motion.div className="w-3 h-3 bg-green-500 rounded-full"></motion.div>
                 <h4 className="font-normal text-gray-100 text-l">Hire me.</h4>
               </div>
             </div>
-            <div className="absolute bottom-0 right-0 p-2 mb-2 mr-2 bg-red-500 rounded-xl">
-              INI
-            </div>
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              className="absolute bottom-0 right-0 px-2 py-1 mb-2 mr-2 bg-red-500 rounded-xl"
+            >
+              <button>
+                <i className="text-lg text-white ri-moon-clear-line"></i>
+              </button>
+            </motion.div>
           </div>
         </div>
         <div className="bg-slate-50 w-[100px] h-[100px] rounded-full translate-x-[200px] -translate-y-[130px]">
@@ -27,7 +34,8 @@ function App() {
           />
         </div>
         <div className="mb-10 -mt-[7rem] text-center font-bold text-xl">
-          <h1>Aufa Ramadhan</h1>
+          <h1 className="text-xl">Aufa Ramadhan</h1>
+          <h3 className="text-sm font-normal">Frontend Developer</h3>
         </div>
         <LinkCard />
       </div>
--- src/components/LinkCard.tsx
+++ src/components/LinkCard.tsx
@@ -1,14 +1,21 @@
 import linkcard from "../services/LinkCard.json";
+import { motion } from "framer-motion";
 
 export default function LinkCard() {
   return (
     <>
       {linkcard.map((link, index) => (
         <div className="w-[350px] mx-auto" key={index}>
-          <button className="flex items-center justify-center w-full gap-3 px-10 py-5 mb-6 rounded shadow-md bg-slate-100">
+          <motion.a
+            href={link.link}
+            className="flex items-center justify-center w-full gap-3 px-10 py-5 mb-6 rounded shadow-md bg-slate-200"
+            target="_blank"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
             <i className={`${link.logo} text-2xl`}></i>
             <h3 className="text-xl">{link.title}</h3>
-          </button>
+          </motion.a>
         </div>
       ))}
     </>
